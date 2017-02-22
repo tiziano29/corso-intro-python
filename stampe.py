407e3c84e39cf416d6af98bbb11f582f00e7e2ec
Andrea <andrea.carmisciano@gmail.com>
2017-02-22 16:09:11 +0100

    prime stampe

---
+++ stampe.py
@@ -0,0 +1,17 @@
+# qui sto salutando mamma e nonna
+print "Ciao mamma", "Ciao nonna"
+print 3, 12
+print 3.12 # questo e' un numero in virgola mobile
+print True
+
+print 3 + 10
+print 3 - 4
+print 3 * 2
+print 3 / 2
+print 3 % 2
+
+print 3 > 4
+
+print 3 < 10
+
+
