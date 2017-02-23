e5591ce668a22bc2aba79d90e2fd649bb7e087c1
Andrea Carmisciano <andrea.carmisciano@gmail.com>
2017-02-23 16:01:22 +0100

    Un sacco di stampe

--- stampe.py
+++ stampe.py
@@ -14,4 +14,35 @@ print 3 > 4
 
 print 3 < 10
 
+name = "Andrea"
+age = 34
+delta = 10
+template = "Ciao, mi chiamo %s e ho %s anni"
+
+sentence1 = "Ciao mamma guarda"
+sentence2 = "come mi diverto"
+
+print "Ciao, mi chiamo %s e ho %s anni" % (name, age)
+print "Ciao, sono %s" % name
+
+print "Ciao, mi chiamo %s e 10 anni fa avevo %s anni" % (name, age-10)
+
+print "Ciao, mi chiamo %s e %s anni fa avevo %s anni" % (name, delta, age-delta)
+
+print template % (name, age)
+
+print sentence1 + " " + sentence2 
+
+print """
+Lorem ipsum
+dolor sit amet
+"""
+
+print "Ciao. \n Mi chiamo Andrea \n\n e ho 34 anni."
+
+
+
+
+
+
 
