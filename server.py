d1ed15765478a24b811f043a8e78defa0846c204
tiziano29 <tiziano.ugolini29@gmail.com>
2017-03-13 17:01:55 +0100

    html flaks

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template,request
+from oggetti import Automobile
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,10 +7,11 @@ def hello():
     return "Hello World!"
 
 
-
+"""
 @app.route('/saluta')
 def saluta(nome):
 	return 'ciao %s' % nome
+"""
 
 @app.route('/somma/<int:a>/<int:b>')
 def somma (a, b):
@@ -17,13 +19,36 @@ def somma (a, b):
 	return "%s" % somma
 
 
-@app.route ('auto/<nome>/<anno>/<marca>/<consumo>')
+@app.route ('/auto/<nome>/<anno>/<marca>/<consumo>')
 def crea_automobile(nome, anno, marca, consumo):
 	auto=Automobile(nome, anno ,marca, comsumo)
 	return auto
 
+@app.route('/welcome')
+def welcome():
+	return render_template('welcome.html')
 
 
+@app.route('/saluta', methods=['POST','GET'])
+def saluta():
+	name = request.form['name']
+	return render_template('*saluta.html', name=name)
+
+@app.route8 ('/nuovaauto')
+def nuovaauto():
+	return render_template('nuovauato.html')
+
+@app.route8 ('/salvaauto',method =["POST"])
+def salvaauto():
+	nome = request.form['name']
+	anno = request.form['year']
+	marca = request.form['brand']
+	consumo = request.form['compustion']
+
+	auto = Automobile(nome, anno, marca, consumo)
+	
+	return return render_template('salvauto.html', name = auto,nome)
+	
 
 
 
